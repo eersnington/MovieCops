849e3aa97aab5882f6905c2ff596911ce340c565
Eers <sreeaadhi07@gmail.com>
2023-03-07 00:12:39 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
