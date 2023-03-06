50a10f08311ef5cb3958133d4ab6bf2354a31fef
Eers <sreeaadhi07@gmail.com>
2023-03-07 00:38:09 +0530

    Updates: Added Movie Model

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
+Bootstrap(app)
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Numeric(precision=2, scale=1), nullable=False)
+    tags = db.Column(db.String(50), nullable=False)
+    ticketPrice = db.Column(db.Integer, default=250, nullable=False)
+    language = db.Column(db.String(50), nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
