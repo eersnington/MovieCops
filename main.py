7c74e4c04a875cff0a82d0a092fa7b6955011794
Eers <sreeaadhi07@gmail.com>
2023-03-15 08:46:32 +0530

    Added DB, Tables and Basic Login

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
 Bootstrap(app)
 
+# models for tables
+loggged_in = False
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,11 +20,23 @@ class Movie(db.Model):
     language = db.Column(db.String(50), nullable=False)
 
 
+class User(db.Model):
+    email = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    mobile_no = db.Column(db.Integer, nullable=False, unique=True)
+    language = db.Column(db.String(50), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        searchedTerm = request.form['search']
-        return render_template('shows.html', search=searchedTerm)
+        searched_term = request.form['search']
+        return render_template('shows.html', search=searched_term)
     return render_template('index.html')
 
 
@@ -32,6 +47,18 @@ def shows():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        print(request.data)
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return render_template('login.html', error='User not found')
+        if user.password != password:
+            return render_template('login.html', error='Invalid password')
+        global logged_in
+        logged_in = True
+        return render_template('index.html')
     return render_template('login.html')
 
 
