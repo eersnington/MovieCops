c1490799744099e6a97963be216b49f5d5ec8e9f
Eers <sreeaadhi07@gmail.com>
2023-03-28 18:32:56 +0530

    Search functionality

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ userData = {
 """
 
 
-# TODO: Implement search post in evert endpoint
-
 class Movie(db.Model):
     movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
@@ -89,9 +87,6 @@ class Shows(db.Model):
 def index():
     if logged_in:
         return render_template('index.html', userData=userData)
-    if request.method == "POST":
-        searched_term = request.form['search']
-        return render_template('shows.html', search=searched_term, userData=userData, admin=True)
     return render_template('index.html')
 
 
@@ -114,11 +109,55 @@ def logout():
 @app.route('/shows', methods=['GET', 'POST'])
 def shows():
     movies_list = Movie.query.all()
-    # TODO: SORT SHOWS BASED ON LANGUAGE || IF USER IS SIGNED IN, SELECT CHOSEN LANGUAGE
-    if logged_in and not admin_user:
-        return render_template('shows.html', userData=userData, movies=movies_list)
+    if request.method == "POST":
+        if request.form['submit'] == "search":
+            searched_term = request.form['search']
+            res = Movie.query.filter(Movie.name.contains(searched_term)).all()
+
+            if admin_user:
+                return render_template('shows.html', userData=userData, search=searched_term, results=res,
+                                       movies=movies_list, admin=True)
+            if logged_in:
+                return render_template('shows.html', userData=userData, search=searched_term, results=res,
+                                       movies=movies_list)
+            else:
+                return render_template('shows.html', search=searched_term, results=res, movies=movies_list)
+
+        elif request.form['submit'] == "Filter":
+            min_rating = request.form['minRating']
+            language = request.form['language']
+            location = request.form['location']
+
+            passed = False
+
+            query = db.session.query(Movie, Venue, Shows).filter(Venue.venue_id == Shows.venue_id).filter(Movie.movie_id == Shows.movie_id)
+
+            if location != "Any":
+                query = query.filter(Venue.location == location)
+                passed = True
+            if language != "Any":
+                query = query.filter(Movie.language == language)
+                passed = True
+            if min_rating:
+                query = query.filter(Movie.rating >= min_rating)
+                passed = True
+
+            if passed:
+                res = [x[0] for x in query.all()]
+                if admin_user:
+                    return render_template('shows.html', movies=movies_list, userData=userData, search="Filter",
+                                           results=res, admin=True)
+                if logged_in:
+                    return render_template('shows.html', movies=movies_list, userData=userData, search="Filter",
+                                           results=res)
+                return render_template('shows.html', movies=movies_list, search="Filter", results=res)
+            else:
+                return render_template('shows.html', movies=movies_list, search="Filter")
+
     if admin_user:
-        return render_template('shows.html', userData=userData, movies=movies_list, admin=True)
+        return render_template('shows.html', movies=movies_list, userData=userData, admin=True)
+    if logged_in:
+        return render_template('shows.html', movies=movies_list, userData=userData)
     return render_template('shows.html', movies=movies_list)
 
 
@@ -186,9 +225,6 @@ def signup():
 def admin():
     if not admin_user:
         return redirect(url_for('index'))
-    if request.method == "POST":
-        searched_term = request.form['search']
-        return render_template('shows.html', search=searched_term, userData=userData, admin=True)
     return render_template('admin.html', userData=userData, admin=True)
 
 
@@ -236,7 +272,6 @@ def dashboard():
             db.session.add(new_venue)
             db.session.commit()
         if request.form['submit'] == "Add Show":
-
             movie_name = request.form['movieName']
             movie_rating = request.form['movieRating']
             movie_tags = request.form['movieTags']
@@ -252,7 +287,8 @@ def dashboard():
             ticket_price = request.form['ticketPrice']
             venue_id = request.form['venueId']
 
-            new_show = Shows(movie_id=new_movie.movie_id, venue_id=venue_id, ticket_price=ticket_price, seats=50, timings=movie_timings)
+            new_show = Shows(movie_id=new_movie.movie_id, venue_id=venue_id, ticket_price=ticket_price, seats=50,
+                             timings=movie_timings)
             db.session.add(new_show)
             db.session.commit()
 
